b36c67b50e853df32f1936755f6795a45329d34e
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:36:54 -0400

    fix(ganache/target): clean up and fix ganache/target issues

--- ganache/target/debug/deps/pin_project_lite-76c3808c3fc65c09.d
+++
@@ -1,7 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/deps/pin_project_lite-76c3808c3fc65c09.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pin-project-lite-0.2.16/src/lib.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libpin_project_lite-76c3808c3fc65c09.rlib: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pin-project-lite-0.2.16/src/lib.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libpin_project_lite-76c3808c3fc65c09.rmeta: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pin-project-lite-0.2.16/src/lib.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/pin-project-lite-0.2.16/src/lib.rs:
--- ganache/target/debug/deps/ppv_lite86-75b16608cab4d391.d
+++
@@ -1,9 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/deps/ppv_lite86-75b16608cab4d391.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/soft.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/types.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/mod.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/sse2.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libppv_lite86-75b16608cab4d391.rmeta: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/soft.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/types.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/mod.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/sse2.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/lib.rs:
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/soft.rs:
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/types.rs:
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/mod.rs:
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ppv-lite86-0.2.21/src/x86_64/sse2.rs:

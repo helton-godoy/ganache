c53c0084c40e7c1f06aaa09c5374dc89e3cfd921
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:31:40 -0400

    fix(build): clean up and fix build system issues

--- ganache/target/debug/build/thiserror-01289695c6f27cd2/build_script_build-01289695c6f27cd2.d
+++
@@ -1,5 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/build/thiserror-01289695c6f27cd2/build_script_build-01289695c6f27cd2.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/build.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/build/thiserror-01289695c6f27cd2/build_script_build-01289695c6f27cd2: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/build.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/build.rs:

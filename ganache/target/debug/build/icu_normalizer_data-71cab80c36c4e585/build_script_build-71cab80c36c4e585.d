5847b197335093b29abdca88831cc1e8626a1c9d
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:04:15 -0400

    fix(build): clean up and fix build system issues

--- ganache/target/debug/build/icu_normalizer_data-71cab80c36c4e585/build_script_build-71cab80c36c4e585.d
+++
@@ -1,5 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/build/icu_normalizer_data-71cab80c36c4e585/build_script_build-71cab80c36c4e585.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/icu_normalizer_data-2.1.1/build.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/build/icu_normalizer_data-71cab80c36c4e585/build_script_build-71cab80c36c4e585: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/icu_normalizer_data-2.1.1/build.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/icu_normalizer_data-2.1.1/build.rs:

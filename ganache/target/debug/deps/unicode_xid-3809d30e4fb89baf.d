427b53964acef57d4501681c6edd3a3ebdb43629
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:36:56 -0400

    fix(ganache/target): clean up and fix ganache/target issues

--- ganache/target/debug/deps/unicode_xid-3809d30e4fb89baf.d
+++
@@ -1,8 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/deps/unicode_xid-3809d30e4fb89baf.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-xid-0.2.6/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-xid-0.2.6/src/tables.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libunicode_xid-3809d30e4fb89baf.rlib: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-xid-0.2.6/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-xid-0.2.6/src/tables.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libunicode_xid-3809d30e4fb89baf.rmeta: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-xid-0.2.6/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-xid-0.2.6/src/tables.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-xid-0.2.6/src/lib.rs:
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-xid-0.2.6/src/tables.rs:

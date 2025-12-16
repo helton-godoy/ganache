cb4ddc447b9de133b9ee28e958c847c36047922e
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:03:43 -0400

    fix(build): clean up and fix build system issues

--- ganache/target/debug/build/zstd-sys-7fe3a8ae2e46e3ea/build_script_build-7fe3a8ae2e46e3ea.d
+++
@@ -1,5 +0,0 @@
-/root/GANACHE/ganache/target/debug/build/zstd-sys-7fe3a8ae2e46e3ea/build_script_build-7fe3a8ae2e46e3ea.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zstd-sys-2.0.16+zstd.1.5.7/build.rs
-
-/root/GANACHE/ganache/target/debug/build/zstd-sys-7fe3a8ae2e46e3ea/build_script_build-7fe3a8ae2e46e3ea: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zstd-sys-2.0.16+zstd.1.5.7/build.rs
-
-/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zstd-sys-2.0.16+zstd.1.5.7/build.rs:

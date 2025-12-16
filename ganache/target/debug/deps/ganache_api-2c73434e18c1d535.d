aaf5c59eaf9d569c018d0cb7cd8ee20d04dd7e67
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:35:26 -0400

    fix(ganache/target): clean up and fix ganache/target issues

--- ganache/target/debug/deps/ganache_api-2c73434e18c1d535.d
+++
@@ -1,7 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/deps/ganache_api-2c73434e18c1d535.d: src/ganache-api/src/main.rs src/ganache-api/src/handlers.rs src/ganache-api/src/routes.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libganache_api-2c73434e18c1d535.rmeta: src/ganache-api/src/main.rs src/ganache-api/src/handlers.rs src/ganache-api/src/routes.rs
-
-src/ganache-api/src/main.rs:
-src/ganache-api/src/handlers.rs:
-src/ganache-api/src/routes.rs:
--- ganache/target/debug/deps/ganache_api-bf121b446386ebf7.d
+++
@@ -1,5 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/deps/ganache_api-bf121b446386ebf7.d: src/ganache-api/src/main.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libganache_api-bf121b446386ebf7.rmeta: src/ganache-api/src/main.rs
-
-src/ganache-api/src/main.rs:

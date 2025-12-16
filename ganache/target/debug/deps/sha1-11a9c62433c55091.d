d092c55518a26bac225f5ca4d29cd76d0580b470
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:35:17 -0400

    fix(ganache/target): clean up and fix ganache/target issues

--- ganache/target/debug/deps/sha1-11a9c62433c55091.d
+++
@@ -1,8 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/deps/sha1-11a9c62433c55091.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress/soft.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress/x86.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libsha1-11a9c62433c55091.rmeta: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress/soft.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress/x86.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/lib.rs:
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress.rs:
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress/soft.rs:
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/sha1-0.10.6/src/compress/x86.rs:
--- ganache/target/debug/deps/shlex-68503acf491aba38.d
+++
@@ -1,8 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/deps/shlex-68503acf491aba38.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/bytes.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libshlex-68503acf491aba38.rlib: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/bytes.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libshlex-68503acf491aba38.rmeta: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/bytes.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/lib.rs:
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/shlex-1.3.0/src/bytes.rs:

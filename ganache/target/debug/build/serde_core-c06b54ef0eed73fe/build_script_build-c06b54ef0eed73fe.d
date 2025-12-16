432ce1a59651d255fc48f3bb4dbe549e02214bb0
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:03:54 -0400

    fix(build): clean up and fix build system issues

--- ganache/target/debug/build/serde_core-c06b54ef0eed73fe/build_script_build-c06b54ef0eed73fe.d
+++
@@ -1,5 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/build/serde_core-c06b54ef0eed73fe/build_script_build-c06b54ef0eed73fe.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_core-1.0.228/build.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/build/serde_core-c06b54ef0eed73fe/build_script_build-c06b54ef0eed73fe: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_core-1.0.228/build.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_core-1.0.228/build.rs:
--- ganache/target/debug/build/serde_json-58844f69a4e97594/build_script_build-58844f69a4e97594.d
+++
@@ -1,5 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/build/serde_json-58844f69a4e97594/build_script_build-58844f69a4e97594.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_json-1.0.145/build.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/build/serde_json-58844f69a4e97594/build_script_build-58844f69a4e97594: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_json-1.0.145/build.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_json-1.0.145/build.rs:

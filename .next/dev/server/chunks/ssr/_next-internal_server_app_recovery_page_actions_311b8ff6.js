96c4ffe73802e72362946520f4f492ec76348d29
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 11:57:46 -0400

    chore(config): update Api Spec configuration

---
+++ .next/dev/server/chunks/ssr/_next-internal_server_app_recovery_page_actions_311b8ff6.js
@@ -0,0 +1,7 @@
+module.exports = [
+"[project]/.next-internal/server/app/recovery/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
+
+}),
+];
+
+//# sourceMappingURL=_next-internal_server_app_recovery_page_actions_311b8ff6.js.map

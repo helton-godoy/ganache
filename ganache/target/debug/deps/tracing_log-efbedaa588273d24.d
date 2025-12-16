ad5124eb602a1b84be44b259586673fed6464518
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:36:59 -0400

    fix(ganache/target): clean up and fix ganache/target issues

--- ganache/target/debug/deps/tracing_log-efbedaa588273d24.d
+++
@@ -1,6 +0,0 @@
-/root/GANACHE/ganache/target/debug/deps/tracing_log-efbedaa588273d24.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tracing-log-0.2.0/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tracing-log-0.2.0/src/log_tracer.rs
-
-/root/GANACHE/ganache/target/debug/deps/libtracing_log-efbedaa588273d24.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tracing-log-0.2.0/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tracing-log-0.2.0/src/log_tracer.rs
-
-/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tracing-log-0.2.0/src/lib.rs:
-/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tracing-log-0.2.0/src/log_tracer.rs:

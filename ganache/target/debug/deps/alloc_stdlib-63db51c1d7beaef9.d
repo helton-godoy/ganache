6e6d4bb65f143969f6f7279efd31f02d0d10052e
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:35:20 -0400

    fix(ganache/target): clean up and fix ganache/target issues

--- ganache/target/debug/deps/alloc_stdlib-63db51c1d7beaef9.d
+++
@@ -1,7 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/deps/alloc_stdlib-63db51c1d7beaef9.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/alloc-stdlib-0.2.2/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/alloc-stdlib-0.2.2/src/heap_alloc.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/alloc-stdlib-0.2.2/src/std_alloc.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/liballoc_stdlib-63db51c1d7beaef9.rmeta: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/alloc-stdlib-0.2.2/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/alloc-stdlib-0.2.2/src/heap_alloc.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/alloc-stdlib-0.2.2/src/std_alloc.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/alloc-stdlib-0.2.2/src/lib.rs:
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/alloc-stdlib-0.2.2/src/heap_alloc.rs:
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/alloc-stdlib-0.2.2/src/std_alloc.rs:
--- ganache/target/debug/deps/zerofrom_derive-e43372e74752c314.d
+++
@@ -1,6 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/deps/zerofrom_derive-e43372e74752c314.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-derive-0.1.6/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-derive-0.1.6/src/visitor.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libzerofrom_derive-e43372e74752c314.so: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-derive-0.1.6/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-derive-0.1.6/src/visitor.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-derive-0.1.6/src/lib.rs:
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerofrom-derive-0.1.6/src/visitor.rs:

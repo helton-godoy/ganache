3f4a348da39830eeefd8b1500f3530daaeddfc1a
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:03:35 -0400

    fix(build): clean up and fix build system issues

--- ganache/target/debug/build/serde-ea698d7ec55e678b/build_script_build-ea698d7ec55e678b.d
+++
@@ -1,5 +0,0 @@
-/root/GANACHE/ganache/target/debug/build/serde-ea698d7ec55e678b/build_script_build-ea698d7ec55e678b.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs
-
-/root/GANACHE/ganache/target/debug/build/serde-ea698d7ec55e678b/build_script_build-ea698d7ec55e678b: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs
-
-/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde-1.0.228/build.rs:

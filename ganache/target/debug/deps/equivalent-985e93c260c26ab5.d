c645d993e61c0f9c063da429e5bb4096c5543307
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:36:58 -0400

    fix(ganache/target): clean up and fix ganache/target issues

--- ganache/target/debug/deps/equivalent-985e93c260c26ab5.d
+++
@@ -1,5 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/deps/equivalent-985e93c260c26ab5.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/equivalent-1.0.2/src/lib.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libequivalent-985e93c260c26ab5.rmeta: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/equivalent-1.0.2/src/lib.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/equivalent-1.0.2/src/lib.rs:

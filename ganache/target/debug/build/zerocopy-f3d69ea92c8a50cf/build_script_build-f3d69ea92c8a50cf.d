b7893b1a8b09cb9e840b55775c197c4284fe3688
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:19:35 -0400

    fix(build): clean up and fix build system issues

--- ganache/target/debug/build/zerocopy-f3d69ea92c8a50cf/build_script_build-f3d69ea92c8a50cf.d
+++
@@ -1,5 +0,0 @@
-/root/GANACHE/ganache/target/debug/build/zerocopy-f3d69ea92c8a50cf/build_script_build-f3d69ea92c8a50cf.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerocopy-0.8.31/build.rs
-
-/root/GANACHE/ganache/target/debug/build/zerocopy-f3d69ea92c8a50cf/build_script_build-f3d69ea92c8a50cf: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerocopy-0.8.31/build.rs
-
-/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zerocopy-0.8.31/build.rs:

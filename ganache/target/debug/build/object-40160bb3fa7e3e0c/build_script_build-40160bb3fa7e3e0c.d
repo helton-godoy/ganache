11526e80944810123e00d53282d71de3e0747fb6
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:04:13 -0400

    fix(api): fix API bugs

--- ganache/target/debug/build/object-40160bb3fa7e3e0c/build_script_build-40160bb3fa7e3e0c.d
+++
@@ -1,5 +0,0 @@
-/root/GANACHE/ganache/target/debug/build/object-40160bb3fa7e3e0c/build_script_build-40160bb3fa7e3e0c.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/object-0.37.3/build.rs
-
-/root/GANACHE/ganache/target/debug/build/object-40160bb3fa7e3e0c/build_script_build-40160bb3fa7e3e0c: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/object-0.37.3/build.rs
-
-/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/object-0.37.3/build.rs:

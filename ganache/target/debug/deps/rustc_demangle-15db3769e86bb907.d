406e41c1bdd40de4a46a495244c201a68919b27d
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:35:23 -0400

    fix(ganache/target): clean up and fix ganache/target issues

--- ganache/target/debug/deps/rustc_demangle-15db3769e86bb907.d
+++
@@ -1,7 +0,0 @@
-/root/GANACHE/ganache/target/debug/deps/rustc_demangle-15db3769e86bb907.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.26/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.26/src/legacy.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.26/src/v0.rs
-
-/root/GANACHE/ganache/target/debug/deps/librustc_demangle-15db3769e86bb907.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.26/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.26/src/legacy.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.26/src/v0.rs
-
-/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.26/src/lib.rs:
-/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.26/src/legacy.rs:
-/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rustc-demangle-0.1.26/src/v0.rs:

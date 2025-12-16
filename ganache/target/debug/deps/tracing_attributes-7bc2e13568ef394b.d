a6712b0ddc2ba0e77154489e436534998729ca68
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:36:57 -0400

    fix(ganache/target): clean up and fix ganache/target issues

--- ganache/target/debug/deps/tracing_attributes-7bc2e13568ef394b.d
+++
@@ -1,7 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/deps/tracing_attributes-7bc2e13568ef394b.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tracing-attributes-0.1.31/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tracing-attributes-0.1.31/src/attr.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tracing-attributes-0.1.31/src/expand.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libtracing_attributes-7bc2e13568ef394b.so: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tracing-attributes-0.1.31/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tracing-attributes-0.1.31/src/attr.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tracing-attributes-0.1.31/src/expand.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tracing-attributes-0.1.31/src/lib.rs:
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tracing-attributes-0.1.31/src/attr.rs:
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tracing-attributes-0.1.31/src/expand.rs:

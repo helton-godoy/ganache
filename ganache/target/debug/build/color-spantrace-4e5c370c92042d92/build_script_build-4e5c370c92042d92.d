69252062dfbc1c44daae174f132f6c3b37943251
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:31:41 -0400

    fix(build): clean up and fix build system issues

--- ganache/target/debug/build/color-spantrace-4e5c370c92042d92/build_script_build-4e5c370c92042d92.d
+++
@@ -1,5 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/build/color-spantrace-4e5c370c92042d92/build_script_build-4e5c370c92042d92.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color-spantrace-0.3.0/build.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/build/color-spantrace-4e5c370c92042d92/build_script_build-4e5c370c92042d92: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color-spantrace-0.3.0/build.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color-spantrace-0.3.0/build.rs:
--- ganache/target/debug/build/generic-array-59704c25d6ce6f98/build_script_build-59704c25d6ce6f98.d
+++
@@ -1,5 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/build/generic-array-59704c25d6ce6f98/build_script_build-59704c25d6ce6f98.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/generic-array-0.14.7/build.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/build/generic-array-59704c25d6ce6f98/build_script_build-59704c25d6ce6f98: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/generic-array-0.14.7/build.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/generic-array-0.14.7/build.rs:

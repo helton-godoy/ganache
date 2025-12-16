8312c0839bea095504ebd895ec4ef47fb2e0af63
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:36:52 -0400

    fix(ganache/target): clean up and fix ganache/target issues

--- ganache/target/debug/deps/indenter-2f4bc8fd906bcbcd.d
+++
@@ -1,5 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/deps/indenter-2f4bc8fd906bcbcd.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/indenter-0.3.4/src/lib.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libindenter-2f4bc8fd906bcbcd.rmeta: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/indenter-0.3.4/src/lib.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/indenter-0.3.4/src/lib.rs:
--- ganache/target/debug/deps/jobserver-c866244b65c3ca81.d
+++
@@ -1,9 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/deps/jobserver-c866244b65c3ca81.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/jobserver-0.1.34/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/jobserver-0.1.34/src/error.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/jobserver-0.1.34/src/unix.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libjobserver-c866244b65c3ca81.rlib: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/jobserver-0.1.34/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/jobserver-0.1.34/src/error.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/jobserver-0.1.34/src/unix.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libjobserver-c866244b65c3ca81.rmeta: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/jobserver-0.1.34/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/jobserver-0.1.34/src/error.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/jobserver-0.1.34/src/unix.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/jobserver-0.1.34/src/lib.rs:
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/jobserver-0.1.34/src/error.rs:
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/jobserver-0.1.34/src/unix.rs:

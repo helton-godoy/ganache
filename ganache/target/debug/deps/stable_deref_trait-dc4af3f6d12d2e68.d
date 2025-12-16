ed322b2e2ba6aa35f199509551532a12ec98491d
helton-godoy <chileno.brasil@gmail.com>
2025-12-16 12:37:01 -0400

    fix(ganache/target): clean up and fix ganache/target issues

--- ganache/target/debug/deps/stable_deref_trait-dc4af3f6d12d2e68.d
+++
@@ -1,5 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/deps/stable_deref_trait-dc4af3f6d12d2e68.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stable_deref_trait-1.2.1/src/lib.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libstable_deref_trait-dc4af3f6d12d2e68.rmeta: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stable_deref_trait-1.2.1/src/lib.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/stable_deref_trait-1.2.1/src/lib.rs:
--- ganache/target/debug/deps/synstructure-db62ad001d0373e9.d
+++
@@ -1,8 +0,0 @@
-/home/helton/git/GANACHE/ganache/target/debug/deps/synstructure-db62ad001d0373e9.d: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/synstructure-0.13.2/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/synstructure-0.13.2/src/macros.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libsynstructure-db62ad001d0373e9.rlib: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/synstructure-0.13.2/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/synstructure-0.13.2/src/macros.rs
-
-/home/helton/git/GANACHE/ganache/target/debug/deps/libsynstructure-db62ad001d0373e9.rmeta: /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/synstructure-0.13.2/src/lib.rs /home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/synstructure-0.13.2/src/macros.rs
-
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/synstructure-0.13.2/src/lib.rs:
-/home/helton/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/synstructure-0.13.2/src/macros.rs:

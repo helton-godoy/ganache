9a9f60bef163e48bf6d83036368b0b0bf89bbf57
helton-godoy <chileno.brasil@gmail.com>
2025-12-18 04:44:33 -0400

    chore(config): update Settings configuration

--- .next/server/app/api/trpc/[trpc]/route.js
+++ .next/server/app/api/trpc/[trpc]/route.js
@@ -1,6 +1,6 @@
 var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/trpc/[trpc]/route.js")
-R.c("server/chunks/[root-of-the-server]__5e1fe3b2._.js")
-R.c("server/chunks/node_modules_next_dist_esm_build_templates_app-route_aa22b09c.js")
+R.c("server/chunks/[root-of-the-server]__8482b4a3._.js")
+R.c("server/chunks/[root-of-the-server]__3bfb12ed._.js")
 R.c("server/chunks/[root-of-the-server]__8f5ebbc3._.js")
 R.c("server/chunks/_next-internal_server_app_api_trpc_[trpc]_route_actions_646e8836.js")
 R.m(73972)

6c9f37a7801ce7a2378ce494db79e01533a38c41
helton-godoy <chileno.brasil@gmail.com>
2025-12-18 04:08:05 -0400

    feat(core): add core functionality

---
+++ core/target/debug/deps/libz_rs_sys-7973b9c23f9c44c3.d
@@ -0,0 +1,11 @@
+/root/GANACHE/core/target/debug/deps/libz_rs_sys-7973b9c23f9c44c3.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libz-rs-sys-0.5.4/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libz-rs-sys-0.5.4/src/../README.md
+
+/root/GANACHE/core/target/debug/deps/liblibz_rs_sys-7973b9c23f9c44c3.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libz-rs-sys-0.5.4/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libz-rs-sys-0.5.4/src/../README.md
+
+/root/GANACHE/core/target/debug/deps/liblibz_rs_sys-7973b9c23f9c44c3.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libz-rs-sys-0.5.4/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libz-rs-sys-0.5.4/src/../README.md
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libz-rs-sys-0.5.4/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libz-rs-sys-0.5.4/src/../README.md:
+
+# env-dep:CARGO_PKG_VERSION=0.5.4
+# env-dep:CARGO_PKG_VERSION_MAJOR=0

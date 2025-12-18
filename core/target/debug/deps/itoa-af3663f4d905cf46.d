f5d85a7e8fb1436a8fbf58f4d7d672556692d8f4
helton-godoy <chileno.brasil@gmail.com>
2025-12-18 04:08:28 -0400

    feat(core): add core functionality

---
+++ core/target/debug/deps/itoa-af3663f4d905cf46.d
@@ -0,0 +1,8 @@
+/root/GANACHE/core/target/debug/deps/itoa-af3663f4d905cf46.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs
+
+/root/GANACHE/core/target/debug/deps/libitoa-af3663f4d905cf46.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs
+
+/root/GANACHE/core/target/debug/deps/libitoa-af3663f4d905cf46.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/itoa-1.0.15/src/udiv128.rs:

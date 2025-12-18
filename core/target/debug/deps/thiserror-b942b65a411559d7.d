a909988e66830ecb81114edca77fd41ba28bc545
helton-godoy <chileno.brasil@gmail.com>
2025-12-18 04:08:01 -0400

    feat(core): add core functionality

---
+++ core/target/debug/deps/thiserror-b942b65a411559d7.d
@@ -0,0 +1,14 @@
+/root/GANACHE/core/target/debug/deps/thiserror-b942b65a411559d7.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/aserror.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/display.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/var.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/private.rs /root/GANACHE/core/target/debug/build/thiserror-c4be12b08819dbfd/out/private.rs
+
+/root/GANACHE/core/target/debug/deps/libthiserror-b942b65a411559d7.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/aserror.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/display.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/var.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/private.rs /root/GANACHE/core/target/debug/build/thiserror-c4be12b08819dbfd/out/private.rs
+
+/root/GANACHE/core/target/debug/deps/libthiserror-b942b65a411559d7.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/aserror.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/display.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/var.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/private.rs /root/GANACHE/core/target/debug/build/thiserror-c4be12b08819dbfd/out/private.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/aserror.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/display.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/var.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-2.0.17/src/private.rs:
+/root/GANACHE/core/target/debug/build/thiserror-c4be12b08819dbfd/out/private.rs:
+
+# env-dep:OUT_DIR=/root/GANACHE/core/target/debug/build/thiserror-c4be12b08819dbfd/out

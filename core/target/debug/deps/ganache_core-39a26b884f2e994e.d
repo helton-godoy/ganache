87a7a2371b546b6f00957daa980a86a285550a66
helton-godoy <chileno.brasil@gmail.com>
2025-12-18 04:44:17 -0400

    fix(core): fix core functionality bugs

---
+++ core/target/debug/deps/ganache_core-39a26b884f2e994e.d
@@ -0,0 +1,5 @@
+/root/GANACHE/core/target/debug/deps/ganache_core-39a26b884f2e994e.d: ganache-core/src/main.rs
+
+/root/GANACHE/core/target/debug/deps/ganache_core-39a26b884f2e994e: ganache-core/src/main.rs
+
+ganache-core/src/main.rs:

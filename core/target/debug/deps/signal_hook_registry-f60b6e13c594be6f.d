24c1d0b34de130dde7306556881197699d368a66
helton-godoy <chileno.brasil@gmail.com>
2025-12-18 04:08:02 -0400

    feat(core): add core functionality

---
+++ core/target/debug/deps/signal_hook_registry-f60b6e13c594be6f.d
@@ -0,0 +1,8 @@
+/root/GANACHE/core/target/debug/deps/signal_hook_registry-f60b6e13c594be6f.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/signal-hook-registry-1.4.7/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/signal-hook-registry-1.4.7/src/half_lock.rs
+
+/root/GANACHE/core/target/debug/deps/libsignal_hook_registry-f60b6e13c594be6f.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/signal-hook-registry-1.4.7/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/signal-hook-registry-1.4.7/src/half_lock.rs
+
+/root/GANACHE/core/target/debug/deps/libsignal_hook_registry-f60b6e13c594be6f.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/signal-hook-registry-1.4.7/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/signal-hook-registry-1.4.7/src/half_lock.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/signal-hook-registry-1.4.7/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/signal-hook-registry-1.4.7/src/half_lock.rs:

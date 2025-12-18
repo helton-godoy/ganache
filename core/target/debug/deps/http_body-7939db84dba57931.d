8ebb5d1628ccbc1e8969f1eef8fd87bd78769ed8
helton-godoy <chileno.brasil@gmail.com>
2025-12-18 04:08:29 -0400

    feat(core): add core functionality

---
+++ core/target/debug/deps/http_body-7939db84dba57931.d
@@ -0,0 +1,9 @@
+/root/GANACHE/core/target/debug/deps/http_body-7939db84dba57931.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/http-body-1.0.1/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/http-body-1.0.1/src/frame.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/http-body-1.0.1/src/size_hint.rs
+
+/root/GANACHE/core/target/debug/deps/libhttp_body-7939db84dba57931.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/http-body-1.0.1/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/http-body-1.0.1/src/frame.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/http-body-1.0.1/src/size_hint.rs
+
+/root/GANACHE/core/target/debug/deps/libhttp_body-7939db84dba57931.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/http-body-1.0.1/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/http-body-1.0.1/src/frame.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/http-body-1.0.1/src/size_hint.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/http-body-1.0.1/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/http-body-1.0.1/src/frame.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/http-body-1.0.1/src/size_hint.rs:

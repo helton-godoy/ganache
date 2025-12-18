23b22c9e1528cdfc3e0b2cfec8e4b427597f1f8a
helton-godoy <chileno.brasil@gmail.com>
2025-12-18 04:08:00 -0400

    feat(core): add core functionality

---
+++ core/target/debug/deps/unicase-a984494e0d050d45.d
@@ -0,0 +1,10 @@
+/root/GANACHE/core/target/debug/deps/unicase-a984494e0d050d45.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicase-2.8.1/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicase-2.8.1/src/ascii.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicase-2.8.1/src/unicode/mod.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicase-2.8.1/src/unicode/map.rs
+
+/root/GANACHE/core/target/debug/deps/libunicase-a984494e0d050d45.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicase-2.8.1/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicase-2.8.1/src/ascii.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicase-2.8.1/src/unicode/mod.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicase-2.8.1/src/unicode/map.rs
+
+/root/GANACHE/core/target/debug/deps/libunicase-a984494e0d050d45.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicase-2.8.1/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicase-2.8.1/src/ascii.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicase-2.8.1/src/unicode/mod.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicase-2.8.1/src/unicode/map.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicase-2.8.1/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicase-2.8.1/src/ascii.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicase-2.8.1/src/unicode/mod.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicase-2.8.1/src/unicode/map.rs:
---
+++ core/target/debug/deps/utoipa_swagger_ui-4c0fd1b672e47e98.d
@@ -0,0 +1,14 @@
+/root/GANACHE/core/target/debug/deps/utoipa_swagger_ui-4c0fd1b672e47e98.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/actix.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/axum.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/oauth.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/rocket.rs /root/GANACHE/core/target/debug/build/utoipa-swagger-ui-f974781ee55ed414/out/embed.rs
+
+/root/GANACHE/core/target/debug/deps/libutoipa_swagger_ui-4c0fd1b672e47e98.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/actix.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/axum.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/oauth.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/rocket.rs /root/GANACHE/core/target/debug/build/utoipa-swagger-ui-f974781ee55ed414/out/embed.rs
+
+/root/GANACHE/core/target/debug/deps/libutoipa_swagger_ui-4c0fd1b672e47e98.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/actix.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/axum.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/oauth.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/rocket.rs /root/GANACHE/core/target/debug/build/utoipa-swagger-ui-f974781ee55ed414/out/embed.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/actix.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/axum.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/oauth.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/utoipa-swagger-ui-9.0.2/src/rocket.rs:
+/root/GANACHE/core/target/debug/build/utoipa-swagger-ui-f974781ee55ed414/out/embed.rs:
+
+# env-dep:OUT_DIR=/root/GANACHE/core/target/debug/build/utoipa-swagger-ui-f974781ee55ed414/out
